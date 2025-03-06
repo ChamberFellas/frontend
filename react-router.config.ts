7c655c112690ec8e3044694722caa9423281f1c8
Max London <grahamluk66@gmail.com>
2025-03-06 13:12:14 +0000

    [1] Removed boilerplate

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,15 @@
 import type { Config } from "@react-router/dev/config";
 
+// Todo: Look into this to optimize data fetching
+// https://reactrouter.com/start/framework/data-loading
+
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  future: {
+    // Test stability of experimental features
+    unstable_optimizeDeps: true,
+    unstable_splitRouteModules: true,
+  },
 } satisfies Config;
